a715b338bc5bf22da7981db0d8d9ac28f0f73663
chen tong <chen2015tong@gmail.com>
2024-03-08 18:30:37 +0100

    add operators overloading

--- basics/4_operators/CppOperators.cpp
+++ basics/4_operators/CppOperators.cpp
@@ -1,3 +1,11 @@
-//
-// Created by TONG CHEN on 08/03/2024.
-//
+#include <iostream>
+
+int main() {
+    std::string str1 = "test1";
+    std::string str2 = "test2";
+
+    // 关于运算符，正确返回1，错过返回0
+    // 结果可用于if条件语句判断
+    bool result = str1 < str2;
+    std::cout << result << std::endl;
+}
--- basics/4_operators/overloading/CppOverloading.cpp
+++ basics/4_operators/overloading/CppOverloading.cpp
@@ -1,3 +1,66 @@
-//
-// Created by TONG CHEN on 08/03/2024.
-//
+#include <iostream>
+
+struct MyStruct {
+public:
+    int value;
+
+    friend std::ostream& operator<<(std::ostream& os, const MyStruct& myStruct) {
+        return os << myStruct.value;
+    }
+};
+
+
+class DemoClass {
+public:
+    int num;
+    std::string name;
+
+    DemoClass(int num, std::string name) : num(num), name(std::move(name)) {}
+
+    // Equality Operators 相等元算符的重载
+    bool operator==(const DemoClass &rhs) const {
+        return name == rhs.name && num == rhs.num;
+    }
+
+    bool operator!=(const DemoClass &rhs) const {
+        return !(rhs == *this);
+    }
+
+    // Relational Operators 关系运算符的重载
+    // 基于指定属性值的比较，之后的运算符全部基于该比较的结果
+    bool operator<(const DemoClass &rhs) const {
+        return name < rhs.name && num < rhs.num;
+    }
+
+    bool operator>(const DemoClass &rhs) const {
+        return rhs < *this;
+    }
+
+    bool operator<=(const DemoClass &rhs) const {
+        return !(rhs < *this);
+    }
+
+    bool operator>=(const DemoClass &rhs) const {
+        return !(*this < rhs);
+    }
+
+    // Stream Output Operators 重载<<左移操作符
+    // 相当于定义了一个toString()的output输出流
+    friend std::ostream &operator<<(std::ostream &os, const DemoClass &aClass) {
+        os << "num: " << aClass.num;
+        os << "name: " << aClass.name;
+        return os;
+    }
+};
+
+int main() {
+    DemoClass demoClass(1, "name");
+    DemoClass demoClass1(2, "name2");
+
+    bool result1 = demoClass == demoClass1;
+    bool result2 = demoClass < demoClass1;
+
+    // 调用重载的输出流操作符，直接输出对象
+    std::cout << demoClass << std::endl;
+    return 0;
+}
